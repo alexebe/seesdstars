5852f3cbb7198c8548fc7f742cec99206309f94f
auguste.ebe <auguste.ebe@socitech.com>
2017-09-19 20:28:24 +0000

    First commit

---
+++ challenge1.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+from jenkinsapi.jenkins import Jenkins
+import sqlite3
+import datetime from datetime
+
+def get_server_instance(jenkins_url,username,password):
+    server = Jenkins(jenkins_url, username, password)
+    return server
+
+"""Get job details of each job that is running on the Jenkins instance"""
+def get_job_details():
+  url=raw_input("enter jenkins url ")
+  username=raw_input("enter jenkins username") 
+  password=raw_input("enter jenkins password")    
+  server = get_server_instance(url,username,password)
+  conn=databaseConnection()
+  cursor = conn.cursor()
+  for j in server.get_jobs():
+    job_instance = server.get_job(j[0])
+    print 'Job Name:%s' %(job_instance.name)
+    print 'Job Description:%s' %(job_instance.get_description())
+    print 'Is Job running:%s' %(job_instance.is_running())
+    print 'Is Job enabled:%s' %(job_instance.is_enabled())
+    cursor.execute("INSERT INTO jobs(name, status,date) VALUES(?, ?)",
+     (job_instance.name, job_instance.is_running(),datetime.date.year)+"/"+datetime.date.month+"/"+datetime.date.day)
+        
+  conn.commit()
+  conn.close()
+
+
+def databaseConnection():
+  conn = sqlite3.connect('my_base.db') 
+  cursor = conn.cursor()
+  cursor.execute("""
+    CREATE TABLE IF NOT EXISTS jobs(
+     id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+     name TEXT,
+     status INTERGER,
+     date INTEGER
+)
+""")
+  conn.commit()
+  return conn
+
+
+get_job_details()
